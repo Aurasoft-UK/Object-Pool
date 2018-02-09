3ab8dcc4154691ff77031488e80216fe05568a32
SolarPortal <35874037+SolarPortal@users.noreply.github.com>
2018-02-09 15:58:52 +0000

    Update ObjectPool.cpp

--- ObjectPool.cpp
+++ ObjectPool.cpp
@@ -1,1 +1,23 @@
 
+#include "ObjectPool.h"
+
+function runExample()
+{
+  // Initialise the system with
+  objectPool.createItem();
+
+  // Create new item with
+  Item *item = objectPool.createItem();
+
+  // Destroy that item with:
+  objectPool.destroyItem(item->id);
+
+  // Destroy all the items with
+  objectPool.destroyAllItems();
+
+  // Get the number of items found with
+  int numberOfItems = objectPool.getNumItems();
+
+  // Retrieve an item by id
+  Item *item = objectPool.getItem(id);
+}
